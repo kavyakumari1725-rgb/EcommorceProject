1ae5b3797f18acd30ee9d7bf483d6be1505ecacd
kavyakumari1725-rgb <kavyakumari1725@gmail.com>
2025-11-03 00:48:37 +0000

    added monitor and logging

--- shop/settings.py
+++ shop/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'shop.catalog',
     'shop.cart',
     'shop.orders',
+    'django_prometheus',
 ]
 
 MIDDLEWARE = [
@@ -26,6 +27,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'shop.urls'
