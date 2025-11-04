e3f194a0caacebf6709dc0efcb2a0599c81edf80
kavyakumari1725-rgb <kavyakumari1725@gmail.com>
2025-11-04 14:38:40 +0000

    connect to slack to see changes

--- shop/cart/views.py
+++ shop/cart/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.catalog.models import Product
-from .cart import Cart  # ✅ ensure you import your Cart class
+from .cart import Cart
 
 @require_POST
 def cart_add(request, product_id):
